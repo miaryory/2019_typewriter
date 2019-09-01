69647da52d66eea484eb3f9c078506b29d8ff273
miaryory <miar0004@stud.kea.dk>
2019-09-01 13:36:54 +0200

    funcions added

--- typewriter.js
+++ typewriter.js
@@ -1,1 +1,33 @@
 "use strict";
+const text = document.querySelector("h1#typewriter").textContent;
+let initialText = document.querySelector("h1#typewriter");
+let sound1 = document.querySelector("#typekey1");
+let sound2 = document.querySelector("#typekey2");
+let soundSpace = document.querySelector("#typespace");
+
+
+initialText.textContent = "";
+
+window.addEventListener("load", start);
+
+let i = 0;
+
+function start() {
+
+    if (i < text.length) {
+        initialText.textContent += text.charAt(i);
+
+        //adding the sound
+        if (text.charAt(i) == " ") {
+            soundSpace.play();
+        } else if (i % 2 == 0) {
+            sound1.play();
+        } else if (i % 2 != 0) {
+            sound2.play();
+        }
+
+        i++;
+        setTimeout(start, 1000);
+        //Math.random() * (400 - 40) + 40
+    }
+}
